e305777f0480e0975b300a5732878b4d12f18b5c
Wandering-Sayo <sayo_hidaka@hotmail.com>
2017-10-17 20:53:34 -0300

    Uploading program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorteo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaParticipantes = new List<string>();
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine("Para participar del sorteo por favor ingrese su nombre.");
+                string participante = Console.ReadLine();
+                listaParticipantes.Add(participante);
+                Console.WriteLine("Ya está participando!");
+            }
+
+            Console.WriteLine("El sorteo ha finalizado! Esta es la lista de participantes:");
+            
+            foreach(string item in listaParticipantes)
+            {
+                Console.WriteLine(item);
+            }
+            int ganador = new Random().Next(listaParticipantes.Count+1);
+            Console.WriteLine("El ganador es " + listaParticipantes[ganador] + "! Felicitaciones!!!!!");
+
+        }
+    }
+}
